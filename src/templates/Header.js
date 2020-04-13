5e69104a2fff1fff8276a7c28c243913c8275e15
Cristhian Cabezas <cristhiancabezas9@gmail.com>
2020-04-13 01:10:17 -0500

    Cambios ok con travis script

---
+++ src/templates/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    const view = `
+        <div class="Header-main">
+            <h1>
+                <a href="/">
+                    100tifi.co
+                </a>
+            </h1>
+            <div class="Header-logo">
+                <h1>
+                    <a href="#/about/">
+                        About
+                    </a>
+                </h1>
+            </div>
+        </div>
+    `;
+    return view;
+};
+
+export default Header;
